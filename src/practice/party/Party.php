3b33bb06b4a26e3a1c39c575a8964a13aa285302
JkqzDev <espulefjuan13@gmail.com>
2022-10-21 08:15:56 -0300

    Basic party

--- src/practice/party/Party.php
+++ src/practice/party/Party.php
@@ -4,5 +4,38 @@ declare(strict_types=1);
 
 namespace practice\party;
 
+use pocketmine\player\Player;
+
 class Party {
+
+    public function __construct(
+        private Player $owner,
+        private bool $open = true,
+        private array $members = []
+    ) {
+    }
+
+    public function getOwner(): Player {
+        return $this->owner;
+    }
+
+    public function getMembers(): array {
+        return $this->members;
+    }
+
+    public function isOwner(Player $player): bool {
+        return $player->getXuid() === $this->owner->getXuid();
+    }
+
+    public function isOpen(): bool {
+        return $this->open;
+    }
+
+    public function isMember(Player $player): bool {
+        return isset($this->members[spl_object_hash($player)]);
+    }
+
+    public function addMemeber(Player $player): void {
+        $this->members[spl_object_hash($player)] = $player;
+    }
 }
