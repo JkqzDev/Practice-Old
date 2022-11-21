2b821e4ed651efa0cd2272bf4e6d6bf9b249f0ba
zOmArRD <omarsena25@gmail.com>
2022-11-21 14:15:03 -0400

    Missing if added

--- src/practice/database/mysql/queries/SelectAsync.php
+++ src/practice/database/mysql/queries/SelectAsync.php
@@ -18,6 +18,11 @@ final class SelectAsync extends QueryAsync {
         ?\Closure $onComplete = null,
         string    $columns = '*',
     ) {
+        if ($conditions === []) {
+            parent::__construct(sprintf('SELECT %s FROM %s %s;', $columns, $table, $_extra), $onComplete);
+            return;
+        }
+
         $where = implode(' AND ', array_map(static fn($key, $value) => "{$key} = '{$value}'", array_keys($conditions), array_values($conditions)));
         parent::__construct(sprintf('SELECT %s FROM %s WHERE %s %s;', $columns, $table, $where, $_extra), $onComplete);
     }
